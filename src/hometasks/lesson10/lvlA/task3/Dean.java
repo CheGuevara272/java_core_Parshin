1b374d510737af32644a7573c66ac2bc0caff5ee
Parshin Evgeny <Gravity272@gmail.com>
2023-12-25 13:55:34 +0300

    Lesson 10 - Removed trash comments

--- src/hometasks/lesson10/lvlA/task3/Dean.java
+++ src/hometasks/lesson10/lvlA/task3/Dean.java
@@ -15,26 +15,6 @@ public class Dean {
     public static void main(String[] args) {
         Group group = new Group();
         fillGroup(group);
-//        group.addStudent(new Pair<>("John", generateNumber()));
-//        group.addStudent(new Pair<>("Rick", generateNumber()));
-//        group.addStudent(new Pair<>("Luther", generateNumber()));
-//        group.addStudent(new Pair<>("Kenny", generateNumber()));
-//        group.addStudent(new Pair<>("Richy", generateNumber()));
-//        group.addStudent(new Pair<>("Stewie", generateNumber()));
-//        group.addStudent(new Pair<>("Peter", generateNumber()));
-//        group.addStudent(new Pair<>("Eugene", generateNumber()));
-//        group.addStudent(new Pair<>("Gwen", generateNumber()));
-//        group.addStudent(new Pair<>("Miles", generateNumber()));
-//        group.addStudent(new Pair<>("Ahsoka", generateNumber()));
-//        group.addStudent(new Pair<>("Luke", generateNumber()));
-//        group.addStudent(new Pair<>("Anakin", generateNumber()));
-//        group.addStudent(new Pair<>("Aerith", generateNumber()));
-//        group.addStudent(new Pair<>("Eris", generateNumber()));
-//        group.addStudent(new Pair<>("Cloud", generateNumber()));
-//        group.addStudent(new Pair<>("Tifa", generateNumber()));
-//        group.addStudent(new Pair<>("Rogue", generateNumber()));
-//        group.addStudent(new Pair<>("Geralt", generateNumber()));
-//        group.addStudent(new Pair<>("Triss", generateNumber()));
 
         group.printGroup();
     }
