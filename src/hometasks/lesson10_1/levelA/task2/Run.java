e6baa62a01724427285117a4e2b00612b68bfdef
Parshin Evgeny <Gravity272@gmail.com>
2023-12-28 15:38:32 +0300

    Lesson 10.1 level A task 2 - Fully done

---
+++ src/hometasks/lesson10_1/levelA/task2/Run.java
@@ -0,0 +1,19 @@
+package hometasks.lesson10_1.levelA.task2;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Run {
+    public static void main(String[] args) {
+        Map<String, Float> group = new TreeMap<>();
+        group.put("Gwen", 9.81f);
+        group.put("Topson", 7.69f);
+        group.put("Nikita", 8.23f);
+        group.put("Hiness", 7.56f);
+        group.put("Slava", 8.40f);
+
+        for (Map.Entry<String, Float> entry : group.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+}
