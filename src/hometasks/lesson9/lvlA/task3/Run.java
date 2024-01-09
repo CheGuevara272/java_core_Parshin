84dfc087892bf4e83c5da926e587387af6c39733
Parshin Evgeny <Gravity272@gmail.com>
2024-01-09 13:30:03 +0300

    Lesson 9 - added task description

--- src/hometasks/lesson9/lvlA/task3/Run.java
+++ src/hometasks/lesson9/lvlA/task3/Run.java
@@ -1,5 +1,10 @@
 package hometasks.lesson9.lvlA.task3;
 
+/*Создать класс NumberArray, который может хранить в себе массив только из 5 числовых типов.
+  Реализовать методы setElement и getElement которые будут сохранять и получать элементы по заданному индексу.
+  Реализовать метод printArray, который будет выводить в консоль все данные хранящиеся в этом классе.
+  Создать класс Run и в методе main создать объект класса NumberArray сохранив в него 5 чисел, выведите эти числа в консоль.*/
+
 public class Run {
     public static void main(String[] args) {
         NumberArray numberArray = new NumberArray();
