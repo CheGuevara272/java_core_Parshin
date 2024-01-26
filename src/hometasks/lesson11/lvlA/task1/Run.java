377889266ec4e979ddff1e01c25519c6c2d44fdf
Parshin Evgeny <Gravity272@gmail.com>
2024-01-26 15:38:36 +0300

    Lesson 11 level A task 1 - fully done

---
+++ src/hometasks/lesson11/lvlA/task1/Run.java
@@ -0,0 +1,23 @@
+package hometasks.lesson11.lvlA.task1;
+
+import java.util.Random;
+
+public class Run {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Double mark = random.nextDouble() * 5;
+        try {
+            checkMark(mark);
+        } catch (StudentException exception) {
+            System.err.println(exception.getMessage());
+        } finally {
+            System.out.println("Спасибо за участие, ваша оценка: " + mark);
+        }
+    }
+
+    public static void checkMark(Double mark) throws StudentException {
+        if (mark < 4) {
+            throw new StudentException("Студент не сдал экзамен");
+        }
+    }
+}
---
+++ src/hometasks/lesson11/lvlA/task1/StudentException.java
@@ -0,0 +1,7 @@
+package hometasks.lesson11.lvlA.task1;
+
+public class StudentException extends Exception {
+    public StudentException(String message) {
+        super(message);
+    }
+}
