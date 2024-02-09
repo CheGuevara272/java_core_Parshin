b931e08fa0ffea230148935d5c1927e56c3a48d7
Parshin Evgeny <Gravity272@gmail.com>
2024-02-09 14:43:48 +0300

    Lesson 11 - added task description

--- src/hometasks/lesson11/lvlA/task1/Run.java
+++ src/hometasks/lesson11/lvlA/task1/Run.java
@@ -2,6 +2,12 @@ package hometasks.lesson11.lvlA.task1;
 
 import java.util.Random;
 
+/*Создать класс StudentException и отнаследоваться от класса Exception.
+  Для реализации использовать функционал методов родительского класса.
+  Создайте класс Run и в методе main создайте код для генерации случайной оценки.
+  Сделайте проверку оценки, если оценка ниже 4, то сгенерировать исключение
+  StudentException и вывести информации о том, что студент не сдал экзамен.*/
+
 public class Run {
     public static void main(String[] args) {
         Random random = new Random();
--- src/hometasks/lesson11/lvlB/task1/Run.java
+++ src/hometasks/lesson11/lvlB/task1/Run.java
@@ -2,6 +2,14 @@ package hometasks.lesson11.lvlB.task1;
 
 import java.util.concurrent.TimeUnit;
 
+/*Создайте класс NotEnoughMoneyException, который наследуется от Exception.
+  Создайте класс BankAccount с переменной balance которая будет проинициализирована значением 150.
+  Создайте класс Run и в методе main создайте объект класса BankAccount.
+  Смоделируйте ситуацию покупок товаров стоимостью от 5 до 100.
+  Совершать покупки до тех пор, пока баланс не станет меньше 5. В случае, если товар стоит больше,
+  чем сумма на балансе, то сгенерировать исключение и сообщить это пользователю.
+  После каждой покупки выводить в консоль информацию о состоянии баланса.*/
+
 public class Run {
     public static void main(String[] args) {
         BankAccount myBankAccount = new BankAccount();
