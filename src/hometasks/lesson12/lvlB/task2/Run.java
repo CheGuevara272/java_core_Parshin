9c57ecac388188d070e8e4b45ac59245168ea889
Parshin Evgeny <Gravity272@gmail.com>
2024-02-12 15:40:26 +0300

    Lesson 12 level B task 2 - fully done

---
+++ src/hometasks/lesson12/lvlB/task2/Run.java
@@ -0,0 +1,41 @@
+package hometasks.lesson12.lvlB.task2;
+
+import java.io.*;
+
+public class Run {
+    private static final String DIR = "../java_core_group00/src/hometasks/lesson12/lvlB/task1";
+
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        readText();
+        long endTime = System.currentTimeMillis();
+        System.out.println("Total execution time of fileReader: " + (endTime - startTime) + '\n');
+        startTime = System.currentTimeMillis();
+        readBufferedText();
+        endTime = System.currentTimeMillis();
+        System.out.println("Total execution time of bufferedReader: " + (endTime - startTime));
+    }
+
+    private static void readText() {
+        try (Reader fileReader = new FileReader(new File(DIR, "versh.txt"))) {
+            int code;
+            while ((code = fileReader.read()) != -1) {
+                System.out.print(Character.toChars(code));
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static void readBufferedText() {
+        try (FileReader fileReader = new FileReader(new File(DIR, "bufVersh.txt"));
+             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
